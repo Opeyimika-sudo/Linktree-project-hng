95b93a4eae330e84ab64ed6dffbaa07feaf74e85
Opeyimika-sudo <54380080+Opeyimika-sudo@users.noreply.github.com>
2022-10-30 09:34:21 +0100

    init: build the links section structure

--- src/components/Links.js
+++ src/components/Links.js
@@ -3,7 +3,7 @@ import React from "react"
 export default function Links() {
     return (
         <div className="links">
-            <button className="btn btn-1" id="twitter_link"><a href="">Twitter Link</a></button>
+            <button className="btn btn-1" id="twitter_link"><a href="https://twitter.com/LifeOfOpeyimika">Twitter Link</a></button>
             <button className="btn btn-2" id="btn-zuri"><a href="https://training.zuri.team/">Zuri Team</a></button>
             <button className="btn btn-3" id="books"><a href="http://books.zuri.team">Zuri Books</a></button>
             <button className="btn btn-4" id="book_python"><a href="https://books.zuri.team/python-for-beginners?ref_id=opeyimika">Python Books</a></button>
