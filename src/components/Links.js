1c45b337fea32507317bbae242fa049f73a9f8fe
Opeyimika-sudo <54380080+Opeyimika-sudo@users.noreply.github.com>
2022-10-30 09:14:19 +0100

    init: create the structure

--- src/components/Links.js
+++ src/components/Links.js
@@ -3,12 +3,12 @@ import React from "react"
 export default function Links() {
     return (
         <div className="links">
-            <button className="btn btn-1">Twitter Link</button>
-            <button className="btn btn-2">Zuri Team</button>
-            <button className="btn btn-3">Zuri Books</button>
-            <button className="btn btn-4">Python Books</button>
-            <button className="btn btn-5">Background Check for Coders</button>
-            <button className="btn btn-6">Design Books</button>
+            <button className="btn btn-1" id="twitter_link"><a href="">Twitter Link</a></button>
+            <button className="btn btn-2" id="btn-zuri"><a href="https://training.zuri.team/">Zuri Team</a></button>
+            <button className="btn btn-3" id="books"><a href="http://books.zuri.team">Zuri Books</a></button>
+            <button className="btn btn-4" id="book_python"><a href="https://books.zuri.team/python-for-beginners?ref_id=opeyimika">Python Books</a></button>
+            <button className="btn btn-5" id="pitch"><a href="https://background.zuri.team">Background Check for Coders</a></button>
+            <button className="btn btn-6" id="book__design"><a href="https://books.zuri.team/design-rules">Design Books</a></button>
         </div>
     )
 }
