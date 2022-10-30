3e83369eccd34286cacc8934f46089528777adb9
Opeyimika-sudo <54380080+Opeyimika-sudo@users.noreply.github.com>
2022-10-30 11:56:11 +0100

    refactor: edit the Socials section structure

--- src/components/Socials.js
+++ src/components/Socials.js
@@ -5,8 +5,14 @@ import github_icon from '../images/github-icon.png'
 export default function Socials() {
   return (
     <div className="socials">
-      <img src={slack_icon} alt="slack icon"/>
-      <img src={github_icon} alt="github icon"/>
+      <div className="socials-icons">
+        
+        <img src={slack_icon} alt="slack icon" id="slack_icon"/>
+        <a href="https://github.com/Opeyimika-sudo">
+          <img src={github_icon} alt="github icon" id="github_icon"/>
+        </a>
+      </div>
+      <hr></hr>
     </div>
   )
 }
