be4336c9d0ca1d01b33542cf7ee46034df0f531e
Opeyimika-sudo <54380080+Opeyimika-sudo@users.noreply.github.com>
2022-10-30 11:56:58 +0100

    refactor: edit the Footer structure for desktop and mobile

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -1,15 +1,13 @@
 import React from 'react'
-import zuri_icon from '../images/zuri-icon.png'
-import red_dot from '../images/red-dot-zuri.png'
+import zuri_logo from '../images/zuri-logo.svg'
 import I4G from '../images/I4G.png'
 
 export default function Footer() {
   return (
     <div className='footer'>
-        <img src={zuri_icon} alt="zuri icon" />
-        <img src={red_dot} alt="red dot" />
-        <p>HNG Internship 9 Frontend Task</p>
-        <img src={I4G} alt="Ingressive For Good logo" />
+        <img src={zuri_logo} alt="zuri logo" id="zuri_logo"/>
+        <p id="footer_text">HNG Internship 9 Frontend Task</p>
+        <img src={I4G} alt="Ingressive For Good logo" id="I4G_logo" />
     </div>
   )
 }
