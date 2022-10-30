90a6c198f34c871c39c0efd7a1fa50faf20934da
Opeyimika-sudo <54380080+Opeyimika-sudo@users.noreply.github.com>
2022-10-30 11:54:47 +0100

    refactor: edit the initial structure

--- src/components/Profile.js
+++ src/components/Profile.js
@@ -1,11 +1,18 @@
 import React from 'react'
-import profile_img from '../images/profile__img.png'
-import share_icon from '../images/share_button_mobile.png'
+import profile_img from '../images/opeyimika_selfie.jpg'
+import share_icon_desktop from '../images/share_button_desktop.png'
+import share_icon_mobile from '../images/share_button_mobile.png'
 export default function Profile() {
+  const browserWidth = window.innerWidth;
+  console.log(browserWidth)
+  console.log({share_icon_desktop})
+
   return (
     <div className="profile">
-      <img src={share_icon} alt="share profile icon"/>
-      <img src={profile_img} alt="profile" id="profile__img"/>
+      <div className="profile_images">
+        <img src={browserWidth >= 800 ? share_icon_desktop : share_icon_mobile} alt="share profile icon" id="share-icon"/>
+        <img src={profile_img} alt="profile" id="profile__img"/>
+      </div>
       <h2 id="twitter">@LifeOfOpeyimika</h2>
       <p id="slack" style={{display:"none"}}>opeyimika</p>
     </div>
