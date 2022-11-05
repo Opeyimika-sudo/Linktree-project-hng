8b83f44047b29e98cb6e2f5aae919aa752c49915
Opeyimika-sudo <54380080+Opeyimika-sudo@users.noreply.github.com>
2022-11-05 12:27:26 +0100

    feat: add clickable button to a Contact page

---
+++ src/components/Contactme.js
@@ -0,0 +1,12 @@
+import React from 'react'
+import '../App.css'
+
+const Contactme = () => {
+    return (
+        <div className="App">
+            <button className="btn btn-7" id="contact_me">
+                <Link to="../Contact">Contact Me</Link>
+            </button>
+        </div>
+    )
+}
