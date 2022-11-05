183696983199dd60cc5095388af6ea87de42f4cb
Opeyimika-sudo <54380080+Opeyimika-sudo@users.noreply.github.com>
2022-11-05 12:34:08 +0100

    feat: create a Contact page with needed components

---
+++ src/pages/Contact.js
@@ -0,0 +1,13 @@
+import React from 'react'
+import Form from '../components/Form'
+import Footer from '../components/Footer'
+import '../App.css'
+
+export default function Contact() {
+  return (
+    <div className='App'>
+      <Form />
+      <Footer />
+    </div>
+  )
+}
