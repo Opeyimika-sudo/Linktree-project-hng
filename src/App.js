c8594ad69e18605447594db3e263e5c953d0d227
Opeyimika-sudo <54380080+Opeyimika-sudo@users.noreply.github.com>
2022-11-05 12:35:26 +0100

    refactor: add React-Router-DOM for page routing and update page structuring

--- src/App.js
+++ src/App.js
@@ -1,17 +1,23 @@
 import './App.css';
-import Profile from "./components/Profile"
-import Links from "./components/Links"
-import Socials from "./components/Socials"
-import Footer from "./components/Footer"
+// import Profile from "./components/Profile"
+// import Links from "./components/Links"
+// import Socials from "./components/Socials"
+// import Footer from "./components/Footer"
+import Home from './pages/Home'
+import Contact from './pages/Contact'
+import { BrowserRouter, Routes, Route} from 'react-router-dom'
 
 function App() {
   return (
-    <div className="App">
-      <Profile />
-      <Links />
-      <Socials />
-      <Footer />
-    </div>
+    <BrowserRouter>
+      <Routes>
+        <Route path='/' index element={<Home />}>
+        </Route>
+        <Route path='/contact' element={<Contact />}>
+
+        </Route>
+      </Routes>
+    </BrowserRouter>
   );
 }
 
