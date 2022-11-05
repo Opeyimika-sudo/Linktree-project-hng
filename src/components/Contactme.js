b5af4d759238af5e7c4fbe77b691fe4c87b1aafd
Opeyimika-sudo <54380080+Opeyimika-sudo@users.noreply.github.com>
2022-11-05 14:32:51 +0100

    fix: adjust the React router

--- src/components/Contactme.js
+++ src/components/Contactme.js
@@ -1,12 +1,16 @@
 import React from 'react'
 import '../App.css'
-import { Link } from 'react-router-dom'
+import { Link, Outlet } from 'react-router-dom'
 
 export default function Contactme() {
   return (
+    <>
         <button className="btn btn-7" id="contact_me">
                 <Link to="../Contact">Contact Me</Link>
         </button>
+        <Outlet />
+    </>
   )
 } 
+
 
