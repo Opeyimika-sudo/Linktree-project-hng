61a99814109582918f700c0aae16791b0d46c60b
Opeyimika-sudo <54380080+Opeyimika-sudo@users.noreply.github.com>
2022-11-06 16:57:19 +0100

    update: edit the form with required tag

--- src/components/Form.js
+++ src/components/Form.js
@@ -25,6 +25,7 @@ export default function Form() {
     function handleSubmit(event){
         event.preventDefault();
         console.log(formData)
+        formData !== null ? alert("Form filled") : alert("Fill the form please!");
         setFormData({ firstName: "", lastName: "", message: "", email: "", isChecked: false})
     }
 
@@ -37,18 +38,45 @@ export default function Form() {
                 <div className="names">
                     <div className="first-name">
                         <label htmlFor="first_name" id="first">First name</label><br />
-                        <input type="text" name="firstName" onChange= {handleChange} value={formData.firstName} id="first_name" placeholder="Enter your first name" required />
+                        <input 
+                        type="text" 
+                        name="firstName" 
+                        onChange= {handleChange} 
+                        value={formData.firstName} 
+                        id="first_name" 
+                        placeholder="Enter your first name" 
+                        required />
                     </div>
                     <div className="last-name">
                         <label htmlFor="last_name" id="second">Last name</label><br />
-                        <input  type="text" name="lastName" onChange= {handleChange} value={formData.lastName} id="last_name" placeholder="Enter your last name" required />
+                        <input  
+                        type="text" 
+                        name="lastName" 
+                        onChange= {handleChange} 
+                        value={formData.lastName} 
+                        id="last_name" 
+                        placeholder="Enter your last name" 
+                        required />
                     </div>
                 </div>
 
                 <label htmlFor="email">Email</label>
-                <input type="email" name="email" onChange={handleChange} value={formData.email} id="email" placeholder="yourname@email.com" required />
+                <input 
+                type="email" 
+                name="email" 
+                onChange={handleChange} 
+                value={formData.email} 
+                id="email" 
+                placeholder="yourname@email.com" 
+                required />
                 <label htmlFor="message">Message</label>
-                <textarea name="message" onChange={handleChange} value={formData.message} id="message" placeholder="Send me a message and I'll reply you as soon as possible..." required />
+                <textarea 
+                name="message" 
+                onChange={handleChange} 
+                value={formData.message} 
+                id="message" 
+                placeholder="Send me a message and I'll reply you as soon as possible..." 
+                required />
                 
                 <div className="checkbox">
                     <input type="checkbox" name="isChecked" onChange={handleChange} checked={formData.isChecked} id="isChecked"/>
